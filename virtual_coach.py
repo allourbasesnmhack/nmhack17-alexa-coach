7bc1dd1245f1ff1ba9697ea90bb6a2c284f086c7
Scott Seifert <scottseifert@northwesternmutual.com>
2017-04-27 15:11:05 -0500

    push local

--- virtual_coach.py
+++ virtual_coach.py
@@ -1,5 +1,6 @@
 import logging
 import boto3
+from boto3.dynamodb.conditions import Key, Attr
 #from random import randint
 from flask import Flask, render_template
 from flask_ask import Ask, statement, question, session
@@ -18,12 +19,20 @@ response = userinfo_table.get_item(
 userinfo_item = response['Item']
 
 
+activities_table = dynamodb.Table('activities')
+response = activities_table.scan(FilterExpression=boto3.dynamodb.conditions.Attr('userid').eq(1))
+activities_items = response['Items']
+print activities_items
+count= 0
+for activity_item in activities_items:
+    count = count + activity_item['count']
+    print count
 
 
 @ask.launch
 def start_skill():
     #welcome_message = render_template('welcome', first_name=item['first_name'], last_name=item['last_name'], openact=item['openact'], factfind=item['factfind'], suspects=item['suspects'], meals=item['meals'])
-    welcome_message = render_template('welcome', first_name=userinfo_item['first_name'], last_name=userinfo_item['last_name'])#, openact=count)
+    welcome_message = render_template('welcome', first_name=userinfo_item['first_name'], last_name=userinfo_item['last_name'], openact=count)
     return question(welcome_message)
 
 @ask.intent("YesGoalsIntent")
