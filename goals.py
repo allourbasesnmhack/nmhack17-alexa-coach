0adb4ebcf86f9965e85ff835f0ac941621102ed2
higginsd <hig9090@s220193.jetpack>
2017-04-27 15:04:37 -0500

    goals

--- goals.py
+++ goals.py
@@ -1,11 +1,20 @@
-def getGoalsMessage(userid):
+import boto3
+from flask import render_template
+from boto3.dynamodb.conditions import Key, Attr
+
+def generateGoalsMessage(userid):
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
     table = dynamodb.Table('goals')
-    response = table.query(
-        KeyConditionExpression=Key('userid').eq(userid)
-    )
+    response = table.scan(FilterExpression=boto3.dynamodb.conditions.Attr("userid").eq(userid))
+
     items = response['Items']
     goal_message= ""
     for item in items:
-        diff= item['target']-item['actual']
-        goal_message+= render_template('goal_item', goal_name=item['goalname'],actual=item['actual'],target=item['target'],difference=diff)
+        print item['goalname']
+        diff= int(item['target'])-int(item['actual'])
+        if(diff <= 0):
+            diff=diff*-1
+            goal_message+= render_template('goal_item_over', goalname=item['description'],actual=item['actual'],target=item['target'],difference=diff)
+        else:
+            goal_message+= render_template('goal_item_under', goalname=item['description'],actual=item['actual'],target=item['target'],difference=diff)
     return goal_message
