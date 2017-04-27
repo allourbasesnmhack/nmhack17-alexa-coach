7163b2aa61fec4f3a0f96bf5a0d9df5ea76035b7
Scott Seifert <scottseifert@northwesternmutual.com>
2017-04-27 12:08:33 -0500

    base working

--- virtual_coach.py
+++ virtual_coach.py
@@ -1,6 +1,6 @@
 import logging
 import boto3
-from random import randint
+#from random import randint
 from flask import Flask, render_template
 from flask_ask import Ask, statement, question, session
 
@@ -8,42 +8,33 @@ app = Flask(__name__)
 ask = Ask(app, "/")
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+table = dynamodb.Table('users')
+response = table.get_item(
+    Key={
+        'username': 'normweston',
+        'last_name': 'Weston'
+    }
+)
+item = response['Item']
+
 @ask.launch
 def start_skill():
-    #welcome_message = render_template('welcome')
-
-    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-    table = dynamodb.Table('users')
-
-    response = table.get_item(
-        Key={
-            'username': 'normweston',
-            'last_name': 'Weston'
-        }
-    )
-
-    item = response['Item']
-    #firstname = item['first_name']
-    #print(item['first_name'])
-    #welcome_message = item['first_name']
-    welcome_message = 'Welcome ' + item['first_name'] + ' ' + item['last_name'] + ' to the North Western Mutual Virtual Coach.'
-    welcome_message = welcome_message + '''
-                      Here is your daily summary of activity.
-                      Norm, you have a total of 5 open activities for today.
-                      You have 3 fact finding.
-                      You have 2 qualified suspect.
-                      Would you like to hear your goals tracking?
-                      '''
+    welcome_message = render_template('welcome', first_name=item['first_name'], last_name=item['last_name'], openact=item['openact'], factfind=item['factfind'], suspects=item['suspects'], meals=item['meals'])
     return question(welcome_message)
 
-@ask.intent("YesIntent")
+@ask.intent("YesGoalsIntent")
 def yes_intent():
-    yes_message = 'Norm, you are over you goals by 5 activities'
-    return statement(yes_message)
+    if item['openact'] > 10:
+        goals_message = render_template('goals_good', first_name=item['first_name'], openact=item['openact'])
+    else:
+        goals_message = render_template('goals_bad', first_name=item['first_name'], openact=item['openact'])
+    # goals_message = render_template('tester')
+    return question(goals_message)
 
-@ask.intent("NoIntent")
+@ask.intent("NoGoalsIntent")
 def no_intent():
-    no_message = 'See you tomorrow.  Have a good day!'
+    no_message = render_template('good_bye')
     return statement(no_message)
 
 if __name__ == '__main__':
